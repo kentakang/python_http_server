b97f1c929f4ff44cb97b71a0229b1178ee2f2efd
Chan Kang <kentakang@icloud.com>
2018-09-10 18:45:10 +0900

    Support .jpg

--- server.py
+++ server.py
@@ -1,11 +1,12 @@
 import socket
 import socketserver
 import os.path
+import base64
 from datetime import datetime
 
 server_name = "Python Light-Weight Server"
 date_formatting = "%a, %d %b %Y %H:%M:%S KST"
-home_dir = os.path.expanduser("~") + "\webroot"
+home_dir = os.path.expanduser("~") + "\\webroot"
 
 class ServerHandler(socketserver.BaseRequestHandler):
     def handle(self):
@@ -18,14 +19,30 @@ class ServerHandler(socketserver.BaseRequestHandler):
         request_object = home_dir + request_data[1]
         request_version = request_data[2]
 
+        sendEncoded = True
+
         if (request_object[len(request_object) - 1] == '/'):
             request_object += "index.html"
 
         if request_method == "GET":
             if (os.path.exists(request_object)):
-                file = open(request_object, 'r', encoding='utf8')
-                file_content = file.read()
-                response_data = "{0} 200 OK\nServer: {1}\nDate: {2}\n\n{3}".format(request_version, server_name, datetime.now().strftime(date_formatting), file_content)
+                response_object = request_object.split('.')
+                
+                if (response_object[1] == 'html'):
+                    file = open(request_object, 'r', encoding='utf8')
+                    file_content = file.read()
+                    response_data = "{0} 200 OK\nServer: {1}\nDate: {2}\n\n{3}".format(request_version, server_name, datetime.now().strftime(date_formatting), file_content)
+                    file.close()
+
+                elif (response_object[1] == 'jpg'):
+                    file = open(request_object, "rb")
+                    file_size = os.path.getsize(request_object)
+                    file_content = file.read(file_size)
+                    response_data = "{0} 200 OK\nServer: {1}\nDate: {2}\nContent-type: image/jpeg\nContent-length: {3}\n\n".format(request_version, server_name, datetime.now().strftime(date_formatting), file_size)
+                    socket.send(response_data.encode())
+                    socket.send(file_content)
+                    sendEncoded = False
+                    file.close()
 
             else:
                 response_data = "{0} 404 Not Found\nServer: {1}\nDate: {2}\n\n404 Not Found".format(request_version, server_name, datetime.now().strftime(date_formatting))
@@ -33,8 +50,9 @@ class ServerHandler(socketserver.BaseRequestHandler):
             response_data = "{0} 405 Method Not Allowed\nServer: {1}\nDate: {2}\n".format(request_version, server_name, 
             datetime.now().strftime('%a, %d %b %Y %H:%M:%S KST'))
 
-        socket.send(response_data.encode())
-        print('송신 : {0}'.format(response_data))
+        if sendEncoded:
+            socket.send(response_data.encode())
+        
         socket.close()
 
 if __name__ == '__main__':
