35f144908d8c76322f3315450cab24f6b32ce2c3
Chan Kang <kentakang@icloud.com>
2018-09-10 16:33:38 +0900

    Add server.py

---
+++ server.py
@@ -0,0 +1,42 @@
+import socket
+import socketserver
+import os.path
+from datetime import datetime
+
+server_name = "Python Light-Weight Server"
+date_formatting = "%a, %d %b %Y %H:%M:%S KST"
+home_dir = os.path.expanduser("~") + "\webroot"
+
+class ServerHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        socket = self.request
+
+        data = socket.recv(65535)
+        request_data = data.decode().split()
+
+        request_method = request_data[0]
+        request_object = home_dir + request_data[1]
+        request_version = request_data[2]
+
+        if (request_object[len(request_object) - 1] == '/'):
+            request_object += "index.html"
+
+        if request_method == "GET":
+            if (os.path.exists(request_object)):
+                file = open(request_object, 'r', encoding='utf8')
+                file_content = file.read()
+                response_data = "{0} 200 OK\nServer: {1}\nDate: {2}\n\n{3}".format(request_version, server_name, datetime.now().strftime(date_formatting), file_content)
+
+            else:
+                response_data = "{0} 404 Not Found\nServer: {1}\nDate: {2}\n\n404 Not Found".format(request_version, server_name, datetime.now().strftime(date_formatting))
+        else:
+            response_data = "{0} 405 Method Not Allowed\nServer: {1}\nDate: {2}\n".format(request_version, server_name, 
+            datetime.now().strftime('%a, %d %b %Y %H:%M:%S KST'))
+
+        socket.send(response_data.encode())
+        print('송신 : {0}'.format(response_data))
+        socket.close()
+
+if __name__ == '__main__':
+    server = socketserver.TCPServer(('localhost', 12345), ServerHandler)
+    server.serve_forever()
