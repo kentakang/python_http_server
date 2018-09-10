a9b3ea4c6ee1db516789f6b23238d31e3b043dfb
Chan Kang <kentakang@icloud.com>
2018-09-10 19:30:30 +0900

    Support CSS/JS

--- server.py
+++ server.py
@@ -8,6 +8,9 @@ server_name = "Python Light-Weight Server"
 date_formatting = "%a, %d %b %Y %H:%M:%S KST"
 home_dir = os.path.expanduser("~") + "\\webroot"
 
+image_list = ['gif', 'jpg', 'jpeg', 'png', 'svg']
+image_mime = ['image/gif', 'image/jpeg', 'image/jpeg', 'image/png', 'image/svg']
+
 class ServerHandler(socketserver.BaseRequestHandler):
     def handle(self):
         socket = self.request
@@ -21,31 +24,46 @@ class ServerHandler(socketserver.BaseRequestHandler):
 
         sendEncoded = True
 
-        if (request_object[len(request_object) - 1] == '/'):
+        if request_object[len(request_object) - 1] == '/':
             request_object += "index.html"
 
         if request_method == "GET":
             if (os.path.exists(request_object)):
                 response_object = request_object.split('.')
                 
-                if (response_object[1] == 'html'):
+                if response_object[1] == 'html':
                     file = open(request_object, 'r', encoding='utf8')
                     file_content = file.read()
-                    response_data = "{0} 200 OK\nServer: {1}\nDate: {2}\n\n{3}".format(request_version, server_name, datetime.now().strftime(date_formatting), file_content)
+                    response_data = "{0} 200 OK\nServer: {1}\nDate: {2}\nContent-Type: text/html\n\n{3}".format(request_version, server_name, datetime.now().strftime(date_formatting), file_content)
                     file.close()
 
-                elif (response_object[1] == 'jpg'):
+                elif response_object[1] == 'css':
+                    file = open(request_object, 'r', encoding='utf8')
+                    file_content = file.read()
+                    response_data = "{0} 200 OK\nServer: {1}\nDate: {2}\nContent-Type: text/css\n\n{3}".format(request_version, server_name, datetime.now().strftime(date_formatting), file_content)
+                    file.close()
+
+                elif response_object[1] == 'js':
+                    file = open(request_object, 'r', encoding='utf8')
+                    file_content = file.read()
+                    response_data = "{0} 200 OK\nServer: {1}\nDate: {2}\nContent-Type: text/javascript\n\n{3}".format(request_version, server_name, datetime.now().strftime(date_formatting), file_content)
+                    file.close()
+
+                elif response_object[1] in image_list:
                     file = open(request_object, "rb")
                     file_size = os.path.getsize(request_object)
                     file_content = file.read(file_size)
-                    response_data = "{0} 200 OK\nServer: {1}\nDate: {2}\nContent-type: image/jpeg\nContent-length: {3}\n\n".format(request_version, server_name, datetime.now().strftime(date_formatting), file_size)
+                    response_data = "{0} 200 OK\nServer: {1}\nDate: {2}\nContent-Type: {3}\nContent-Length: {4}\n\n".format(request_version, server_name, datetime.now().strftime(date_formatting), image_mime[image_list.index(response_object[1])], file_size)
                     socket.send(response_data.encode())
                     socket.send(file_content)
                     sendEncoded = False
                     file.close()
 
             else:
-                response_data = "{0} 404 Not Found\nServer: {1}\nDate: {2}\n\n404 Not Found".format(request_version, server_name, datetime.now().strftime(date_formatting))
+                file = open(home_dir + "/404.html", "r", encoding='utf8')
+                file_content = file.read()
+                response_data = "{0} 404 Not Found\nServer: {1}\nDate: {2}\n\n{3}".format(request_version, server_name, datetime.now().strftime(date_formatting), file_content)
+                file.close()
         else:
             response_data = "{0} 405 Method Not Allowed\nServer: {1}\nDate: {2}\n".format(request_version, server_name, 
             datetime.now().strftime('%a, %d %b %Y %H:%M:%S KST'))
